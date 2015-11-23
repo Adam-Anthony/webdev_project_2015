4e053c0dfcd9ea30745d68db85daf854227cdc10
AdamAnthony <adam.a.pacheco@gmail.com>
2015-11-23 11:01:17 -0500

    Updated files and creating a global CSS file
    Easier for us to work with our colour scheme.
    Ran through editing of our code so far and setting up goals.

--- stats.js
+++ stats.js
@@ -1,8 +1,7 @@
-window.onload = function() {
+window.onload = function () {
 	var options = "";
-
 	for (var i = 4; i < 21; i++) {
-		if (i==10) {
+		if (i===10) {
 			options += "<option selected=\"selected\">"+i+"</option>";
 		} else {
 			options += "<option value="+i+">"+i+"</option>";
@@ -16,18 +15,30 @@ window.onload = function() {
 	document.getElementById("wisSelect").innerHTML = options;
 	document.getElementById("chaSelect").innerHTML = options;
 	
-    document.getElementById("modhead").innerHTML = "JUST";
-	document.getElementById("strSelectMod").innerHTML = "DO";
-	document.getElementById("dexSelectMod").innerHTML = "IT";
-	document.getElementById("conSelectMod").innerHTML = "JUST DO IT";
-	document.getElementById("intSelectMod").innerHTML = "DON'T LET YOUR DREAMS";
-	document.getElementById("wisSelectMod").innerHTML = "BECOME";
-	document.getElementById("chaSelectMod").innerHTML = "DREAMS";
-	
-	
+    document.getElementById("modhead").innerHTML = "Modifier";
+	document.getElementById("strSelectMod").innerHTML = "+0";
+	document.getElementById("dexSelectMod").innerHTML = "+0";
+	document.getElementById("conSelectMod").innerHTML = "+0";
+	document.getElementById("intSelectMod").innerHTML = "+0";
+	document.getElementById("wisSelectMod").innerHTML = "+0";
+	document.getElementById("chaSelectMod").innerHTML = "+0";
 	
 }
 
+//Implement a system to check the points and make sure they don't go over before proceeding to next phase.
+function nextStep(thisMod) {
+    //Grab the values and map out the total spent points
+    if (true){
+        //Next Page
+        //Placeholder
+        document.getElementById("incorrectPoints").innerHTML = "Good";
+    }else{
+        document.getElementById("incorrectPoints").innerHTML = "You spent too many points";
+    }
+}
+
+//Called when changing the stat values
+//Maybe add another column to show "points" spent, and a total of points spent to make it easy to track.
 function modClick(thisMod) {
     var modVal = parseInt((thisMod.options[thisMod.selectedIndex].value-10)/2);
     if (modVal >= 0){
