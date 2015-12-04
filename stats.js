f80b8c635befe8af291492b8bfaa42d74382e777
AdamAnthony <adam.a.pacheco@gmail.com>
2015-12-03 19:37:27 -0500

    Added QoL changes to make system status visual

--- stats.js
+++ stats.js
@@ -24,56 +24,51 @@ window.onload = function () {
 	document.getElementById("wisSelectMod").innerHTML = "+0";
 	document.getElementById("chaSelectMod").innerHTML = "+0";
     
+    var temp;
     if (sessionStorage.getItem("str") !== null){
+        temp = sessionStorage.getItem("str");
         document.getElementById('strSelect').value = sessionStorage.getItem("str");
+        document.getElementById('strSelectMod').innerHTML = convertModValue(temp);
+        document.getElementById('strSelectPoints').innerHTML = convertPointBuy(temp);
     }
     if (sessionStorage.getItem("dex") !== null){
+        temp = sessionStorage.getItem("dex");
         document.getElementById('dexSelect').value = sessionStorage.getItem("dex");
+        document.getElementById('dexSelectMod').innerHTML = convertModValue(temp);
+        document.getElementById('dexSelectPoints').innerHTML = convertPointBuy(temp);
     }
     if (sessionStorage.getItem("wis") !== null){
+        temp = sessionStorage.getItem("wis");
         document.getElementById('wisSelect').value = sessionStorage.getItem("wis");
+        document.getElementById('wisSelectMod').innerHTML = convertModValue(temp);
+        document.getElementById('wisSelectPoints').innerHTML = convertPointBuy(temp);
     }
     if (sessionStorage.getItem("int") !== null){
-        document.getElementById('intSelect').value = sessionStorage.getItem("int");
+        temp = sessionStorage.getItem("int");
+        document.getElementById('intSelect').value = temp;
+        document.getElementById('intSelectMod').innerHTML = convertModValue(temp);
+        document.getElementById('intSelectPoints').innerHTML = convertPointBuy(temp);
     }
     if (sessionStorage.getItem("cha") !== null){
-        document.getElementById('chaSelect').value = sessionStorage.getItem("cha");
+        temp = sessionStorage.getItem("cha");
+        document.getElementById('chaSelect').value = temp;
+        document.getElementById("chaSelectMod").innerHTML = convertModValue(temp);
+        document.getElementById("chaSelectPoints").innerHTML = convertPointBuy(temp);
     }
     if (sessionStorage.getItem("con") !== null){
+        temp = sessionStorage.getItem("con");
         document.getElementById('conSelect').value = sessionStorage.getItem("con");
+        document.getElementById('conSelectMod').innerHTML = convertModValue(temp);
+        document.getElementById('conSelectPoints').innerHTML = convertPointBuy(temp);
     }
+    document.getElementById("incorrectPoints").innerHTML = "You have spent " + getPointsSpent() + "/27 points";
 }
 
 //Implement a system to check the points and make sure they don't go over before proceeding to next phase.
 function nextStep(thisMod) {
     //Grab the values and map out the total spent points
-    var points = 0;
+    var points = getPointsSpent();
     
-    var tempObj, tempValue;
-    temp = document.getElementById("strSelect");
-    tempValue = temp.options[ temp.selectedIndex ].value;
-    points += convertPointBuy(tempValue);
-    
-    temp = document.getElementById("dexSelect");
-    tempValue = temp.options[ temp.selectedIndex ].value;
-    points += convertPointBuy(tempValue);
-    
-    temp = document.getElementById("chaSelect");
-    tempValue = temp.options[ temp.selectedIndex ].value;
-    points += convertPointBuy(tempValue);
-    
-    temp = document.getElementById("intSelect");
-    tempValue = temp.options[ temp.selectedIndex ].value;
-    points += convertPointBuy(tempValue);
-    
-    temp = document.getElementById("wisSelect");
-    tempValue = temp.options[ temp.selectedIndex ].value;
-    points += convertPointBuy(tempValue);
-    
-    temp = document.getElementById("conSelect");
-    tempValue = temp.options[ temp.selectedIndex ].value;
-    
-    points += convertPointBuy(tempValue);
     if (points == 27){
         location.href = "equipment.html";
     }else if (points <= 27){
@@ -83,13 +78,14 @@ function nextStep(thisMod) {
             location.href = "equipment.html";
         }
     }else{
-        document.getElementById("incorrectPoints").innerHTML = "You spent too many points (" + points + ")";
+        document.getElementById("incorrectPoints").innerHTML = "You spent too many points (" + points + "/27)";
     }
 }
 
 function backStep(modval){
     location.href= "classStats.html";
 }
+
 function convertPointBuy(value){
     if(value==8){ return 0;}
     if(value==9){ return 1;}
@@ -109,12 +105,33 @@ function convertPointBuy(value){
 function modClick(thisMod) {
     var rawVal = thisMod.options[thisMod.selectedIndex].value;
     sessionStorage.setItem(thisMod.id.substr(0,3), rawVal);
-    var modVal = parseInt((rawVal-10)/2);
-    if (modVal >= 0 && rawVal >=10){
-	   document.getElementById(thisMod.id+"Mod").innerHTML = "+" + modVal;
+    var modVal = convertModValue(rawVal)
+    document.getElementById(thisMod.id+"Mod").innerHTML = modVal;
+    document.getElementById(thisMod.id+"Points").innerHTML = convertPointBuy(rawVal);
+    
+    document.getElementById("incorrectPoints").innerHTML = ('You have spent ' + getPointsSpent() + '/27 points');
+    
+}
+
+function getPointsSpent(){
+    var points = 0;
+    points += parseInt(document.getElementById("strSelectPoints").innerHTML);
+    points += parseInt(document.getElementById("dexSelectPoints").innerHTML);
+    points += parseInt(document.getElementById("wisSelectPoints").innerHTML);
+    points += parseInt(document.getElementById("intSelectPoints").innerHTML);
+    points += parseInt(document.getElementById("chaSelectPoints").innerHTML);
+    points += parseInt(document.getElementById("conSelectPoints").innerHTML);
+    return points;
+}
+
+function convertModValue(value){
+    var modReturn = 0;
+    var modVal = parseInt((value-10)/2);
+    if (modVal >= 0 && value >=10){
+	   modReturn = "+" + modVal;
     }else{
         modVal = parseInt(modVal/2) - 1;
-        document.getElementById(thisMod.id+"Mod").innerHTML = "" + modVal;
+        modReturn = "" + modVal;
     }
-    document.getElementById(thisMod.id+"Points").innerHTML = convertPointBuy(rawVal);
+    return modReturn;
 }
